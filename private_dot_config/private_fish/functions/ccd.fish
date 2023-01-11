a9a4c2262b9b68ff19548081c2e0de1a85a57920
amtoaer <amtoaer@gmail.com>
2023-01-12 01:54:55 +0800

    Updated: 2023-01-12 01:54.55

--- private_dot_config/private_fish/functions/ccd.fish
+++ private_dot_config/private_fish/functions/ccd.fish
@@ -1,3 +1,3 @@
-function ccd --wraps='chezmoi cd' --description 'alias ccd=chezmoi cd'
-  chezmoi cd $argv; 
+function ccd --wraps='chezmoi cd' --wraps='cd $(chezmoi source-path)' --description 'alias ccd=cd $(chezmoi source-path)'
+  cd $(chezmoi source-path) $argv; 
 end
