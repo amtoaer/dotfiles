7062c08edd9e5995f4bb1453346d054b3b4ac04f
amtoaer <amtoaer@gmail.com>
2023-06-23 12:45:47 +0800

    Updated: 2023-06-23 12:45.47

--- private_dot_config/private_fish/functions/pld.fish
+++ private_dot_config/private_fish/functions/pld.fish
@@ -1,6 +1,6 @@
 function pld --wraps=cd\ \ \&\&\ git\ add\ ./\ \&\&\ git\ commit\ -m\ \'2023/6/23\'\ \&\&\ git\ push
 if set -q GOLEETCODE
-cd $GOLEETCODE && git commit -m (date +%Y/%-m/%d) && git push
+cd $GOLEETCODE && git commit -m (date +%y/%-m/%d) && git push
 else
 echo "GOLEETCODE variable not set!"
 return
