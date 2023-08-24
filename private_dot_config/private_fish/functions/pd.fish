649fabfa836bd92c4bf5f773ef21d20d7e943fbc
amtoaer <amtoaer@gmail.com>
2023-08-24 17:25:51 +0800

    Updated: 2023-08-24 17:25.51

--- private_dot_config/private_fish/functions/pd.fish
+++
@@ -1,4 +0,0 @@
-function pd --wraps='export KUBECONFIG=/home/amtoaer/Documents/production/kubeconfig.json' --description 'alias pd export KUBECONFIG=/home/amtoaer/Documents/production/kubeconfig.json'
-  export KUBECONFIG=/home/amtoaer/Documents/production/kubeconfig.json $argv
-        
-end
